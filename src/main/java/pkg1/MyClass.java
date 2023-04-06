b67d529cb18d4c5742a7fa911003f832c52d037a
Vidhant007 <thapavidhant@gmail.com>
2023-04-07 01:27:34 +0530

    Commit 1

---
+++ src/main/java/pkg1/MyClass.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class MyClass {
+	
+	public void show() {
+		System.out.println("Welcome to Jenkins");
+	}
+	
+	public static void main(String args[]) {
+		MyClass obj = new MyClass();
+		obj.show();
+	}
+
+}
